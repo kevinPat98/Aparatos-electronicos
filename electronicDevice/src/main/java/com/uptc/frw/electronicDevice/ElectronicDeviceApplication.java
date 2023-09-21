03540c1532e75969162793b01ba5846e788e6c7c
klingerDiego <klingerdiaz@gmail.com>
2023-09-21 14:03:50 -0500

    Base del proyecto

---
+++ electronicDevice/src/main/java/com/uptc/frw/electronicDevice/ElectronicDeviceApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.electronicDevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicDeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicDeviceApplication.class, args);
+	}
+
+}
---
+++ electronicDevice/src/test/java/com/uptc/frw/electronicDevice/ElectronicDeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.electronicDevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicDeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
